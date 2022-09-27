2351c52c81a7c169455ddd9793c0b01ddb5bf739
Dionysis Athinaios <d.athinaios@gmail.com>
2022-09-27 17:12:21 +0100

    change SCRIPTPATH discovery method

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
-# Absolute path to this script
-SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in
-SCRIPTPATH=$(dirname "$SCRIPT")
+TARGET=$0
+cd $(dirname "$TARGET")
+TARGET=$(basename "$TARGET")
+# Iterate down a (possible) chain of symlinks
+while [ -L "$TARGET" ]
+do
+    TARGET=$(readlink "$TARGET")
+    cd $(dirname "$TARGET")
+    TARGET=$(basename "$TARGET")
+done
+# Compute the canonicalized name by finding the physical path 
+# for the directory we're in and appending the target file.
+DIR=`pwd -P`
+SCRIPTPATH="$DIR"
 # Link the QT PlugIns
 export QT_PLUGIN_PATH="./QT_PlugIns"
 
